1769f78f19dcdbd86b60d68b07a248c2315b5524
Ippei Sumida <ippey.s@gmail.com>
2019-08-26 17:44:51 +0900

    add get sections request

---
+++ src/Request/Accounting/SectionsRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace Ippey\FreeeUtil\Request\Accounting;
+
+
+use Ippey\FreeeUtil\Request\AccountingRequestInterface;
+use Ippey\FreeeUtil\Traits\AccountingRequestTrait;
+use Ippey\FreeeUtil\Traits\GetOptionsTrait;
+use Ippey\FreeeUtil\Traits\RequestHeaderTrait;
+
+class SectionsRequest implements AccountingRequestInterface
+{
+    use RequestHeaderTrait;
+    use AccountingRequestTrait;
+    use GetOptionsTrait;
+
+    private $company_id;
+
+    private $parameterKeys = [
+        'company_id',
+    ];
+
+    private $options = [];
+
+    public function getUrl()
+    {
+        return 'https://api.freee.co.jp/api/1/sections';
+    }
+}
